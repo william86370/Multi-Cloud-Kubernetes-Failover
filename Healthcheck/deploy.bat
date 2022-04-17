363df5ab614946d591652c61605f1b95ca8e4196
William Wright <william86370@gmail.com>
2022-04-17 11:22:33 -0400

    Cloudwatch-Healthcheck Microservice Added Dockerfile Builds the Micro Flask Webapi main.py Contains the Code for the Micro Flask Service

---
+++ Healthcheck/deploy.bat
@@ -0,0 +1,2 @@
+docker build . -t ghcr.io/william86370/cloudwatch-daemon:latest
+docker push ghcr.io/william86370/cloudwatch-daemon:latest
--- Healthcheck/main.py
+++ Healthcheck/main.py
@@ -10,30 +10,42 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "OK"
+def default():
+    return "CloudWatch-Healthcheck is running Try /healthcheck"
 
 
-@app.route("/get_cloud_provider")
-def get_cloud_provider():
-    """
-    Using Low Level API to get cloud provider
-    :return: cloud provider
-    Use IMDSv2 to get cloud provider
-    """
-    path = 'latest/meta-data/instance-id'
-    url = f'http://169.254.169.254/{path}'
-    r = requests.get(url)
-    return r.content.decode('utf-8')
+#
+# @app.route("/get_cloud_provider")
+# def get_cloud_provider():
+#     """
+#     Using Low Level API to get cloud provider
+#     :return: cloud provider
+#     Use IMDSv2 to get cloud provider
+#     """
+#     # Check if cloud provider is AWS
+#     try:
+#         path = 'latest/dynamic/instance-identity/document'
+#         url = f'http://169.254.169.254/{path}'
+#         response = requests.get(url)
+#         if response.status_code == 200:
+#             # Check Availability Zone
+#             availability_zone = response.json()['availabilityZone']
+#             if availability_zone.startswith('us-east-'):
+#                 return 'AWS'
+#             else:
+#                 return 'Azure'
+#     except:
+#         return "Unknown"
 
 
-@app.route('/healthz')
-def healthz():
+@app.route('/healthcheck')
+def healthcheck():
     return "OK"
 
 
 if __name__ == "__main__":
-    print("Starting CloutWatch-Healthcheck Deamonset")
+    print("Starting CloudWatch-Healthcheck")
     # Get port from environment variable or choose 8080 as local Healthcheck port if no environment variable found
     port = int(os.getenv("PORT", 8080))
+    print(f"Starting Healthcheck on port {port}")
     app.run(host='0.0.0.0', port=port)
