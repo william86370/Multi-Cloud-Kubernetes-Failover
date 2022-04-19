2009b56450e35c4aaacc427191db71b88fbe3cfd
William Wright <william86370@gmail.com>
2022-04-19 13:14:49 -0400

    Update Website Backend Codebase

--- Dashboard/libraries/WebApi.js
+++ Dashboard/libraries/WebApi.js
@@ -7,20 +7,91 @@ const WebAPI_Endpoints = {
     Cluster_status:"api/v1/cluster/status",
 };
 
+
+function update_page(data){
+    //update the page with the data
+    console.log("Updating page with data");
+    // THis is called as a callback for the GET request
+    // update the Text on the page
+    document.getElementById('HEALTH_OK').value = "Not Specified";
+
+
+}
+
+
+// 	"cluster_health": "HEALTH_OK",
+// 	"cluster_health_color": GREEN,
+// 	"nodes_online": "0 Nodes",
+// 	"nodes_online_color": RED,
+// 	"failover_status": "Failover Ready",
+// 	"failover_status_color": GREEN,
+// 	"cloud_provider_aws": "0/2 Deployed",
+// 	"cloud_provider_aws_color": RED,
+// 	"cloud_provider_other": "0/2 Deployed",
+// 	"cloud_provider_other_color": RED,
+// 	"cloud_provider_azure": "0/2 Deployed",
+// 	"cloud_provider_azure_color": RED,
+// 	"cloudwatch_operator": "0/2 Deployed",
+// 	"cloudwatch_operator_color": RED,
+// 	"cloudwatch_dashboard": "0/2 Deployed",
+// 	"cloudwatch_dashboard_color": RED,
+// 	"cloudwatch_Healthcheck": "0/2 Deployed",
+// 	"cloudwatch_Healthcheck_color": RED
+
+
 //--------------------------------------------------------------------------------------------
 function get_cluster_information(Callback){
     //Send Request to get Profile For user
     GetRequest(WebAPI_Endpoints.Cluster_status)
         .then((data) => {
-            if (data.info) {
-                console.log("Cluster Information: " + data.info);
-                if(Callback){
-                    return Callback();
-                }
+            if (data.cluster_health) {
+                console.log("Cluster Information: " + data.cluster_health);
+                console.log("Updating page with data");
+                // Cluster Health
+                //update the Color of the Cluster Health
+                document.getElementById('HEALTH_OK').style.color = data.cluster_health_color;
+                document.getElementById('HEALTH_OK').innerHTML = data.cluster_health;
+
+                // Nodes Online
+                //update the Color of the Nodes Online
+                document.getElementById('n__Nodes').style.color = data.nodes_online_color;
+                document.getElementById('n__Nodes').innerHTML = data.nodes_online;
+                // Failover Status
+                //update the Color of the Failover Status
+                document.getElementById('Failover_Ready').style.color = data.failover_status_color;
+                console.log("Failover Status: " + data.failover_status);
+                console.log("Failover Status Color: " + data.failover_status_color);
+                document.getElementById('Failover_Ready').innerHTML = data.failover_status;
+                //Cloud_Providers
+                //AWS
+                //update the Color of the AWS
+                document.getElementById('n_2_Deployed_ce').style.color = data.cloud_provider_aws_color;
+                document.getElementById('n_2_Deployed_ce').innerHTML = data.cloud_provider_aws;
+                //Azure
+                //update the Color of the Azure
+                document.getElementById('n_2_Deployed_cj').style.color = data.cloud_provider_azure_color;
+                document.getElementById('n_2_Deployed_cj').innerHTML = data.cloud_provider_azure;
+                //Other
+                //update the Color of the Other
+                document.getElementById('n_2_Deployed_cn').style.color = data.cloud_provider_other_color;
+                document.getElementById('n_2_Deployed_cn').innerHTML = data.cloud_provider_other;
+                //CloudWatch_Operator
+                //update the Color of the CloudWatch_Operator
+                document.getElementById('n_2_Deployed').style.color = data.cloudwatch_operator_color;
+                document.getElementById('n_2_Deployed').innerHTML = data.cloudwatch_operator;
+                //CloudWatch_Dashboard
+                //update the Color of the CloudWatch_Dashboard
+                document.getElementById('n_2_Deployed_bc').style.color = data.cloudwatch_dashboard_color;
+                document.getElementById('n_2_Deployed_bc').innerHTML = data.cloudwatch_dashboard;
+                //CloudWatch_Healthcheck
+                //update the Color of the CloudWatch_Healthcheck
+                document.getElementById('n_1_Deployed').style.color = data.cloudwatch_Healthcheck_color;
+                document.getElementById('n_1_Deployed').innerHTML = data.cloudwatch_Healthcheck;
+                //All Text Updated
             } else {
                 console.log("Error: [GET Failed]");
             }
-        })    
+        })
 }
 //--------------------------------------------------------------------------------------------
 async function POSTRequest(API_Endpoint, JSONData) {
--- Dashboard/main.py
+++ Dashboard/main.py
@@ -8,6 +8,9 @@
 
 import json
 from flask import Flask
+# Add CORS support
+from flask_cors import CORS, cross_origin
+
 import os
 from kubernetes import client, config
 import colorama
@@ -21,6 +24,7 @@ global pod_serviceaccount
 global node_name
 global cloud_provider
 global v1_api
+global v1_api_apps
 global port
 
 # Colored output Messages WARNING, ERROR, INFO
@@ -42,6 +46,19 @@ def print_info(message):
     return
 
 
+def print_data(message):
+    print(Fore.BLUE + message)
+    return
+
+
+RED = "#FF0000FF"
+GREEN = "#01BB00FF"
+YELLOW = "#FFFF00FF"
+ORANGE = "#FF7F00FF"
+PURPLE = "#FF00FFFF"
+BLUE = "#0000FFFF"
+WHITE = "#FFFFFFFF"
+
 app = Flask(__name__)
 
 
@@ -54,6 +71,165 @@ def default():
     return information
 
 
+def get_nodes_online():
+    # Get the number of nodes online
+    global v1_api
+    # Get all of the required information from the kubernetes api using v1_api
+    node_list = v1_api.list_node()
+    node_count = 0
+    node_ready = 0
+    node_color = BLUE
+    for node in node_list.items:
+        node_count += 1
+        # Get the node Status
+        node_statuss = node.status.conditions
+        for node_status in node_statuss:
+            if node_status.type == "Ready":
+                if node_status.status == "True":
+                    node_ready += 1
+                    print_info("[INFO] Node " + node.metadata.name + " is online")
+                else:
+                    print_warning("[WARN] Node " + node.metadata.name + " is offline")
+    print_data("[DATA] Nodes Online: " + str(node_count))
+    if node_count == node_ready:
+        print_info("[INFO] All nodes are online")
+        node_color = GREEN
+    elif node_ready == 0:
+        print_warning("[WARN] No nodes are online")
+        node_color = RED
+    else:
+        print_warning("[WARN] Some nodes are offline")
+        node_color = YELLOW
+    node_count = str(node_ready) + "/" + str(node_count)
+    return node_count, node_color
+
+
+def get_cloud_providers():
+    # Get the number of nodes online
+    global v1_api
+    # Get all of the required information from the kubernetes api using v1_api
+    node_list = v1_api.list_node()
+    cloud_provider_count_aws = [0, 0]
+    cloud_provider_count_azure = [0, 0]
+    cloud_provider_count_other = [0, 0]
+    cloud_provider_color_aws = BLUE
+    cloud_provider_color_azure = BLUE
+    cloud_provider_color_other = BLUE
+
+    for node in node_list.items:
+        node_labels = node.metadata.labels
+        # Check if the node is AWS
+        if node_labels.get("cloudwatch/provider") == "aws":
+            cloud_provider_count_aws[0] += 1
+            print_info("[INFO] Node " + node.metadata.name + " is AWS")
+        if node_labels.get("cloudwatch/provider") == "azure":
+            cloud_provider_count_azure[0] += 1
+            print_info("[INFO] Node " + node.metadata.name + " is Azure")
+        if node_labels.get("cloudwatch/provider") == "self-hosted":
+            cloud_provider_count_other[0] += 1
+            print_info("[INFO] Node " + node.metadata.name + " is self-hosted")
+        # Get the node Status
+        node_statuss = node.status.conditions
+        for node_status in node_statuss:
+            if node_status.type == "Ready":
+                if node_status.status == "True":
+                    if node_labels.get("cloudwatch/provider") == "aws":
+                        cloud_provider_count_aws[1] += 1
+                    if node_labels.get("cloudwatch/provider") == "azure":
+                        cloud_provider_count_azure[1] += 1
+                    if node_labels.get("cloudwatch/provider") == "self-hosted":
+                        cloud_provider_count_other[1] += 1
+
+    if cloud_provider_count_aws[1] == 0:
+        print_warning("[WARN] No AWS nodes are online")
+        cloud_provider_color_aws = RED
+    elif cloud_provider_count_aws[0] == cloud_provider_count_aws[1]:
+        print_info("[INFO] All AWS nodes are online")
+        cloud_provider_color_aws = GREEN
+    else:
+        print_warning("[WARN] Some AWS nodes are offline")
+        cloud_provider_color_aws = YELLOW
+    cloud_provider_count_aws = str(cloud_provider_count_aws[1]) + "/" + str(cloud_provider_count_aws[0])
+
+    if cloud_provider_count_azure[1] == 0:
+        print_warning("[WARN] No Azure nodes are online")
+        cloud_provider_color_azure = RED
+    elif cloud_provider_count_azure[0] == cloud_provider_count_azure[1]:
+        print_info("[INFO] All Azure nodes are online")
+        cloud_provider_color_azure = GREEN
+    else:
+        print_warning("[WARN] Some Azure nodes are offline")
+        cloud_provider_color_azure = YELLOW
+    cloud_provider_count_azure = str(cloud_provider_count_azure[1]) + "/" + str(cloud_provider_count_azure[0])
+
+    if cloud_provider_count_other[1] == 0:
+        print_warning("[WARN] No self-hosted nodes are online")
+        cloud_provider_color_other = RED
+    elif cloud_provider_count_other[0] == cloud_provider_count_other[1]:
+        print_info("[INFO] All self-hosted nodes are online")
+        cloud_provider_color_other = GREEN
+    else:
+        print_warning("[WARN] Some self-hosted nodes are offline")
+        cloud_provider_color_other = YELLOW
+    cloud_provider_count_other = str(cloud_provider_count_other[1]) + "/" + str(cloud_provider_count_other[0])
+
+    print_data("[DATA] AWS Nodes Ready: " + str(cloud_provider_count_aws))
+    print_data("[DATA] AZURE Nodes Ready: " + str(cloud_provider_count_azure))
+    print_data("[DATA] OTHER Nodes Ready: " + str(cloud_provider_count_other))
+
+    return cloud_provider_color_aws, cloud_provider_color_azure, cloud_provider_color_other, cloud_provider_count_aws, cloud_provider_count_azure, cloud_provider_count_other
+
+
+def get_deployment_status(deployment_name):
+    global v1_api
+    global pod_namespace
+    global v1_api_apps
+    deployment_status_color = RED
+    deployment_status_count = [0, 0]
+    deployments = v1_api_apps.list_namespaced_deployment(namespace=pod_namespace, label_selector="app=" + deployment_name)
+    for deployment in deployments.items:
+        deployment_status_count[0] += deployment.status.replicas
+        deployment_status_count[1] += deployment.status.ready_replicas
+
+    if deployment_status_count[1] == 0:
+        print_warning("[WARN] No " + deployment_name + " replicas are ready")
+        deployment_status_color = RED
+    elif deployment_status_count[0] == deployment_status_count[1]:
+        print_info("[INFO] All " + deployment_name + " replicas are ready")
+        deployment_status_color = GREEN
+    else:
+        print_warning("[WARN] Some " + deployment_name + " replicas are not ready")
+        deployment_status_color = YELLOW
+    deployment_status_count = str(deployment_status_count[1]) + "/" + str(deployment_status_count[0]) + " Ready"
+    print_data("[DATA] " + deployment_name + " Replicas Ready: " + str(deployment_status_count))
+    return deployment_status_color, deployment_status_count
+
+
+def get_daemon_status(daemon_name):
+    global v1_api
+    global pod_namespace
+    daemon_status_color = RED
+    daemon_status_count = [0, 0]
+
+    daemons = v1_api_apps.list_namespaced_daemon_set(namespace=pod_namespace, label_selector="app=" + daemon_name)
+    for daemon in daemons.items:
+        daemon_status_count[0] += daemon.status.desired_number_scheduled
+        daemon_status_count[1] += daemon.status.number_ready
+
+    if daemon_status_count[0] == 0:
+        print_warning("[WARN] No " + daemon_name + " daemons are ready")
+        daemon_status_color = RED
+    elif daemon_status_count[0] == daemon_status_count[1]:
+        print_info("[INFO] All " + daemon_name + " daemons are ready")
+        daemon_status_color = GREEN
+    else:
+        print_warning("[WARN] Some " + daemon_name + " daemons are not ready")
+        daemon_status_color = YELLOW
+    daemon_status_count = str(daemon_status_count[1]) + "/" + str(daemon_status_count[0]) + " Ready"
+    print_data("[DATA] " + daemon_name + " Daemons Ready: " + str(daemon_status_count))
+    return daemon_status_color, daemon_status_count
+
+
 @app.route("/api/v1/cluster/status")
 def cluster_status():
     # Get the cluster status
@@ -62,29 +238,60 @@ def cluster_status():
     global pod_name
     global pod_namespace
     global pod_serviceaccount
+
+    # Get all of the required information from the kubernetes api using v1_api
+    print("----------------------------------------------------")
     print_info("[INFO] Processing Cluster Status...")
-    RED = "FF0000FF"
-    GREEN = "01BB00FF"
-    INFO = {
-        "cluster_health": "HEALTH_OK",
-        "cluster_health_color": GREEN,
-        "nodes_online": "0 Nodes",
-        "nodes_online_color": RED,
-        "failover_status": "Failover Ready",
-        "failover_status_color": GREEN,
-        "cloud_provider_aws": "0/2 Deployed",
-        "cloud_provider_aws_color": RED,
-        "cloud_provider_other": "0/2 Deployed",
-        "cloud_provider_other_color": RED,
-        "cloud_provider_azure": "0/2 Deployed",
-        "cloud_provider_azure_color": RED,
-        "cloudwatch_operator": "0/2 Deployed",
-        "cloudwatch_operator_color": RED,
-        "cloudwatch_dashboard": "0/2 Deployed",
-        "cloudwatch_dashboard_color": RED,
-        "cloudwatch_Healthcheck": "0/2 Deployed",
-        "cloudwatch_Healthcheck_color": RED
-    }
+
+    INFO = dict(cluster_health="HEALTH_OK", cluster_health_color=GREEN, nodes_online="0 Nodes", nodes_online_color=BLUE,
+                failover_status="Failover Ready", failover_status_color=GREEN, cloud_provider_aws="0/2 Deployed",
+                cloud_provider_aws_color=RED, cloud_provider_other="0/2 Deployed", cloud_provider_other_color=RED,
+                cloud_provider_azure="0/2 Deployed", cloud_provider_azure_color=RED, cloudwatch_operator="0/2 Deployed",
+                cloudwatch_operator_color=RED, cloudwatch_dashboard="0/2 Deployed", cloudwatch_dashboard_color=RED,
+                cloudwatch_Healthcheck="0/2 Deployed", cloudwatch_Healthcheck_color=RED)
+
+    node_online_infd = get_nodes_online()
+    INFO["nodes_online"] = str(node_online_infd[0]) + " Nodes"
+    INFO["nodes_online_color"] = node_online_infd[1]
+
+    # Cloud Providers
+    cloud_providers = get_cloud_providers()
+    INFO["cloud_provider_aws_color"] = cloud_providers[0]
+    INFO["cloud_provider_azure_color"] = cloud_providers[1]
+    INFO["cloud_provider_other_color"] = cloud_providers[2]
+    INFO["cloud_provider_aws"] = cloud_providers[3]
+    INFO["cloud_provider_azure"] = cloud_providers[4]
+    INFO["cloud_provider_other"] = cloud_providers[5]
+
+    # Cloudwatch
+    cloudwatch_operator = get_deployment_status("cloudwatch-operator")
+    INFO["cloudwatch_operator_color"] = cloudwatch_operator[0]
+    INFO["cloudwatch_operator"] = cloudwatch_operator[1]
+    cloudwatch_dashboard = get_deployment_status("cloudwatch-dashboard")
+    INFO["cloudwatch_dashboard_color"] = cloudwatch_dashboard[0]
+    INFO["cloudwatch_dashboard"] = cloudwatch_dashboard[1]
+    cloudwatch_Healthcheck = get_daemon_status("cloudwatch-healthcheck")
+    INFO["cloudwatch_Healthcheck_color"] = cloudwatch_Healthcheck[0]
+    INFO["cloudwatch_Healthcheck"] = cloudwatch_Healthcheck[1]
+
+    # Figure out the cluster health and failover status
+    if INFO["nodes_online_color"] == RED:
+        INFO["cluster_health"] = "HEALTH_WARN"
+        INFO["cluster_health_color"] = ORANGE
+    if INFO["cloud_provider_aws_color"] == RED or INFO["cloud_provider_azure_color"] == RED:
+        INFO["cluster_health"] = "HEALTH_WARN"
+        INFO["cluster_health_color"] = ORANGE
+    if INFO["cloudwatch_operator_color"] == RED or INFO["cloudwatch_Healthcheck_color"] == RED:
+        INFO["cluster_health"] = "HEALTH_WARN"
+        INFO["cluster_health_color"] = ORANGE
+    if INFO["cluster_health_color"] == GREEN:
+        INFO["failover_status"] = "Failover Ready"
+        INFO["failover_status_color"] = GREEN
+    else:
+        INFO["failover_status"] = "Failover Not Ready"
+        INFO["failover_status_color"] = RED
+
+    print("----------------------------------------------------")
     return INFO
 
 
@@ -96,6 +303,7 @@ if __name__ == "__main__":
     global cloud_provider
     global v1_api
     global port
+    global v1_api_apps
     # Check if the Dashboard is in development mode
     # If it is in development mode, it will use the local kubernetes cluster
     print_info("[INFO] Starting Cloudwatch Dashboard V1.0")
@@ -126,6 +334,7 @@ if __name__ == "__main__":
 
     # Cluster will be loaded by now
     v1_api = client.CoreV1Api()
+    v1_api_apps = client.AppsV1Api()
     print_info("[INFO] KubeAPI Successfully Connected to Cluster")
     # Get the current Pod name and namespace
     # if not in development mode
@@ -155,4 +364,5 @@ if __name__ == "__main__":
 
     port = 28015
     print_info(f"Starting Dashboard on port {port}")
+    CORS(app)
     app.run(host='0.0.0.0', port=port)
